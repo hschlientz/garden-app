1b5cdb7301c844580f311dda2c430f1c68882eea
hschlientz <105248652+hschlientz@users.noreply.github.com>
2025-04-25 19:53:07 -0400

    Add files via upload

---
+++ garden_advice.js
@@ -0,0 +1,6 @@
+// TODO: Add a seasonal calendar for planting
+// TODO: Add a watering schedule based on plant type
+// TODO: Take pictures of plants and identify them using an API
+// Suggestion: Add a feature to track plant growth over time
+// Suggestion: Include a community forum for sharing tips and advice
+// Suggestion: Integrate with a weather API to provide localized care tips
